9e6151e21628a25250a11d1cd4d296b6d117ab99
Ishit02422 <22bmiit022@gmail.com>
2026-07-18 18:20:37 +0530

    feat: Keep role as Not Verified after OTP to enable Admin approval workflow

--- Ajax_file/verifymember.php
+++ Ajax_file/verifymember.php
@@ -2,7 +2,7 @@
 
 include "../connect.php";
 session_start();
-$query="update tbl_user set role='Member' where id=".$_SESSION["id"];
+$query="update tbl_user set status=1 where id=".$_SESSION["id"];
 echo mysqli_query($con,$query);
 
 ?>
